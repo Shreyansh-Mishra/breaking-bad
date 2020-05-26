7b27296e4344b15430b73a4999cdbd34e3ffeaf2
Shreyansh-Mishra <45514910+Shreyansh-Mishra@users.noreply.github.com>
2020-05-26 17:06:36 +0530

    Added other pages, and updated index, script.js and style.css

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-
+let yesDeaButton = document.getElementById('dea-yes-button');
+let noDeaButton = document.getElementById('dea-no-button');
+let confirmPage = document.getElementById('confirm')
+let deaPage = document.getElementById('dea-yes')
+let mainPage = document.getElementById('main')
 let walt = document.getElementById('walt');
 let jesse = document.getElementById('jesse');
 let gus = document.getElementById('gus');
 let hank = document.getElementById('hank');
 let saul = document.getElementById('saul');
+
+
+noDeaButton.onclick = () => {
+    mainPage.style.display = "block";
+    confirmPage.style.display = "none";
+
+}
+
+yesDeaButton.onclick = () => {
+    confirmPage.style.display = "none";
+    deaPage.style.display = "block";
+}
+
+
+
+
 walt.onmouseover=(function(){
     let a=document.getElementById('dialogue');
     a.style.transitionDuration='1s';
-    a.style.backgroundColor='orange';
-    a.style.color='black';
-
-
+    a.style.backgroundColor='red';
+    a.style.color='white';
 })
 
 walt.onmouseout=(function(){
@@ -24,8 +42,6 @@ jesse.onmouseover=(function(){
     a.style.transitionDuration='1s';
     a.style.backgroundColor='orange';
     a.style.color='black';
-
-
 })
 
 jesse.onmouseout=(function(){
@@ -37,10 +53,8 @@ jesse.onmouseout=(function(){
 gus.onmouseover=(function(){
     let a=document.getElementById('dialogue3');
     a.style.transitionDuration='1s';
-    a.style.backgroundColor='orange';
-    a.style.color='black';
-
-
+    a.style.backgroundColor='green';
+    a.style.color='white';
 })
 
 gus.onmouseout=(function(){
@@ -52,10 +66,8 @@ gus.onmouseout=(function(){
 hank.onmouseover=(function(){
     let a=document.getElementById('dialogue4');
     a.style.transitionDuration='1s';
-    a.style.backgroundColor='orange';
-    a.style.color='black';
-
-
+    a.style.backgroundColor='#0089D7';
+    a.style.color='white';
 })
 
 hank.onmouseout=(function(){
@@ -67,10 +79,8 @@ hank.onmouseout=(function(){
 saul.onmouseover=(function(){
     let a=document.getElementById('dialogue5');
     a.style.transitionDuration='1s';
-    a.style.backgroundColor='orange';
-    a.style.color='black';
-
-
+    a.style.backgroundColor='purple';
+    a.style.color='white';
 })
 
 saul.onmouseout=(function(){
